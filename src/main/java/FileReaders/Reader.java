c71d1e657dfb60e2cc7943115d947f2d2a0e35f8
jc-johnson <jc-johnson@users.noreply.github.com>
2017-10-24 11:13:06 -0700

    added PorterStemmer algorithm in text file

--- src/main/java/FileReaders/Reader.java
+++ src/main/java/FileReaders/Reader.java
@@ -1,5 +1,7 @@
 package main.java.FileReaders;
 
+import main.java.Utils.Constants;
+
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
@@ -10,15 +12,46 @@ import java.util.List;
 
 public class Reader {
 
+    private List<String> buildStopWords() {
 
+        try {
+
+            List<String> stopWords = new ArrayList<>();
+
+            // Read in file
+            FileInputStream fileInputStream = new FileInputStream(Constants.ALICE_FILE);
+            DataInputStream dataInputStream = new DataInputStream(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dataInputStream));
+            String strLine;
+
+            //Read File Line By Line
+            while ((strLine = bufferedReader.readLine()) != null)   {
+                stopWords.add(strLine);
+                // System.out.println (strLine);
+            }
+
+            for(String string : stopWords) {
+                System.out.println(string);
+            }
+
+            //Close the input stream
+            dataInputStream.close();
+            return stopWords;
+
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        return null;
+    }
 
     public static void main(String[] args) {
         try {
 
-            List<String> fileCopy = new ArrayList<String>();
+            List<String> fileCopy = new ArrayList<>();
 
             // Read in file
-            FileInputStream fileInputStream = new FileInputStream("src/main/java/Resources/alice30.txt");
+            FileInputStream fileInputStream = new FileInputStream(Constants.ALICE_FILE);
             DataInputStream dataInputStream = new DataInputStream(fileInputStream);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dataInputStream));
             String strLine;
