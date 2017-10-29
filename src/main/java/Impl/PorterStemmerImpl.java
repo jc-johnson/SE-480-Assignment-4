f4b9e333fa93f9799026b76bfe287a0f6795a0f1
jc-johnson <jc-johnson@users.noreply.github.com>
2017-10-28 22:45:50 -0700

    reformatting stemmer alg

--- src/main/java/Impl/PorterStemmerImpl.java
+++ src/main/java/Impl/PorterStemmerImpl.java
@@ -234,14 +234,15 @@ public class PorterStemmerImpl {
 
    */
 
-        private final void step1()
-        {  if (b[k] == 's')
-        {  if (ends("sses")) k -= 2; else
-        if (ends("ies")) setto("i"); else
-        if (b[k-1] != 's') k--;
-        }
-            if (ends("eed")) { if (m() > 0) k--; } else
-            if ((ends("ed") || ends("ing")) && vowelinstem())
+        private final void step1() {
+            if (b[k] == 's') {
+                if (ends("sses")) k -= 2;
+                else if (ends("ies")) setto("i");
+                else if (b[k-1] != 's') k--;
+            }
+            if (ends("eed")) {
+                if (m() > 0) k--; }
+                else if ((ends("ed") || ends("ing")) && vowelinstem())
             {  k = j;
                 if (ends("at")) setto("ate"); else
                 if (ends("bl")) setto("ble"); else
@@ -256,16 +257,15 @@ public class PorterStemmerImpl {
             }
         }
 
-   /* step2() turns terminal y to i when there is another vowel in the stem. */
-
+        /* step2() turns terminal y to i when there is another vowel in the stem. */
         private final void step2() { if (ends("y") && vowelinstem()) b[k] = 'i'; }
 
-   /* step3() maps double suffices to single ones. so -ization ( = -ize plus
-      -ation) maps to -ize etc. note that the string before the suffix must give
-      m() > 0. */
-
-        private final void step3() { if (k == 0) return; /* For Bug 1 */ switch (b[k-1])
-        {
+        /* step3() maps double suffices to single ones. so -ization ( = -ize plus
+        -ation) maps to -ize etc. note that the string before the suffix must give
+        m() > 0. */
+        private final void step3() {
+            if (k == 0) return; /* For Bug 1 */
+            switch (b[k-1]) {
             case 'a': if (ends("ational")) { r("ate"); break; }
                 if (ends("tional")) { r("tion"); break; }
                 break;
@@ -296,10 +296,8 @@ public class PorterStemmerImpl {
             case 'g': if (ends("logi")) { r("log"); break; }
         } }
 
-   /* step4() deals with -ic-, -full, -ness etc. similar strategy to step3. */
-
-        private final void step4() { switch (b[k])
-        {
+        /* step4() deals with -ic-, -full, -ness etc. similar strategy to step3. */
+        private final void step4() { switch (b[k]) {
             case 'e': if (ends("icate")) { r("ic"); break; }
                 if (ends("ative")) { r(""); break; }
                 if (ends("alize")) { r("al"); break; }
@@ -313,43 +311,41 @@ public class PorterStemmerImpl {
                 break;
         } }
 
-   /* step5() takes off -ant, -ence etc., in context <c>vcvc<v>. */
-
-        private final void step5()
-        {   if (k == 0) return; /* for Bug 1 */ switch (b[k-1])
-        {  case 'a': if (ends("al")) break; return;
-            case 'c': if (ends("ance")) break;
-                if (ends("ence")) break; return;
-            case 'e': if (ends("er")) break; return;
-            case 'i': if (ends("ic")) break; return;
-            case 'l': if (ends("able")) break;
-                if (ends("ible")) break; return;
-            case 'n': if (ends("ant")) break;
-                if (ends("ement")) break;
-                if (ends("ment")) break;
+        /* step5() takes off -ant, -ence etc., in context <c>vcvc<v>. */
+        private final void step5() {
+            if (k == 0) return; /* for Bug 1 */
+            switch (b[k-1]) {
+                case 'a': if (ends("al")) break; return;
+                case 'c': if (ends("ance")) break;
+                    if (ends("ence")) break; return;
+                case 'e': if (ends("er")) break; return;
+                case 'i': if (ends("ic")) break; return;
+                case 'l': if (ends("able")) break;
+                    if (ends("ible")) break; return;
+                case 'n': if (ends("ant")) break;
+                    if (ends("ement")) break;
+                    if (ends("ment")) break;
                     /* element etc. not stripped before the m */
-                if (ends("ent")) break; return;
-            case 'o': if (ends("ion") && j >= 0 && (b[j] == 's' || b[j] == 't')) break;
-                                    /* j >= 0 fixes Bug 2 */
-                if (ends("ou")) break; return;
+                    if (ends("ent")) break; return;
+                case 'o': if (ends("ion") && j >= 0 && (b[j] == 's' || b[j] == 't')) break;
+                    /* j >= 0 fixes Bug 2 */
+                    if (ends("ou")) break; return;
                     /* takes care of -ous */
-            case 's': if (ends("ism")) break; return;
-            case 't': if (ends("ate")) break;
-                if (ends("iti")) break; return;
-            case 'u': if (ends("ous")) break; return;
-            case 'v': if (ends("ive")) break; return;
-            case 'z': if (ends("ize")) break; return;
-            default: return;
+                case 's': if (ends("ism")) break; return;
+                case 't': if (ends("ate")) break;
+                    if (ends("iti")) break; return;
+                case 'u': if (ends("ous")) break; return;
+                case 'v': if (ends("ive")) break; return;
+                case 'z': if (ends("ize")) break; return;
+                default: return;
         }
             if (m() > 1) k = j;
         }
 
-   /* step6() removes a final -e if m() > 1. */
-
-        private final void step6()
-        {  j = k;
-            if (b[k] == 'e')
-            {  int a = m();
+        /* step6() removes a final -e if m() > 1. */
+        private final void step6() {  j = k;
+            if (b[k] == 'e'){
+                int a = m();
                 if (a > 1 || a == 1 && !cvc(k-1)) k--;
             }
             if (b[k] == 'l' && doublec(k) && m() > 1) k--;
@@ -360,8 +356,8 @@ public class PorterStemmerImpl {
          * from the input.  You can retrieve the result with
          * getResultLength()/getResultBuffer() or toString().
          */
-        public void stem()
-        {  k = i - 1;
+        public void stem(){
+            k = i - 1;
             if (k > 1) { step1(); step2(); step3(); step4(); step5(); step6(); }
             i_end = k+1; i = 0;
         }
@@ -372,44 +368,39 @@ public class PorterStemmerImpl {
          * forcing lower case must be done outside the Stemmer class.
          * Usage: Stemmer file-name file-name ...
          */
-        public static void main(String[] args)
-        {
+        public static void main(String[] args) {
             char[] w = new char[501];
             Stemmer s = new Stemmer();
             for (int i = 0; i < args.length; i++)
-                try
-                {
+                try {
                     FileInputStream in = new FileInputStream(args[i]);
 
-                    try
-                    { while(true)
-
-                    {  int ch = in.read();
-                        if (Character.isLetter((char) ch))
-                        {
+                    try {
+                        while(true) {
+                            int ch = in.read();
+                            if (Character.isLetter((char) ch)) {
                             int j = 0;
-                            while(true)
-                            {  ch = Character.toLowerCase((char) ch);
+                            while(true) {
+                                ch = Character.toLowerCase((char) ch);
                                 w[j] = (char) ch;
                                 if (j < 500) j++;
                                 ch = in.read();
-                                if (!Character.isLetter((char) ch))
-                                {
-                       /* to test add(char ch) */
+                                if (!Character.isLetter((char) ch)) {
+
+                                    /* to test add(char ch) */
                                     for (int c = 0; c < j; c++) s.add(w[c]);
 
-                       /* or, to test add(char[] w, int j) */
-                       /* s.add(w, j); */
+                                    /* or, to test add(char[] w, int j) */
+                                    /* s.add(w, j); */
 
                                     s.stem();
                                     {  String u;
 
-                          /* and now, to test toString() : */
+                                        /* and now, to test toString() : */
                                         u = s.toString();
 
-                          /* to test getResultBuffer(), getResultLength() : */
-                          /* u = new String(s.getResultBuffer(), 0, s.getResultLength()); */
-
+                                        /* to test getResultBuffer(), getResultLength() : */
+                                        /* u = new String(s.getResultBuffer(), 0, s.getResultLength()); */
                                         System.out.print(u);
                                     }
                                     break;
@@ -420,13 +411,13 @@ public class PorterStemmerImpl {
                         System.out.print((char)ch);
                     }
                     }
-                    catch (IOException e)
-                    {  System.out.println("error reading " + args[i]);
+                    catch (IOException e) {
+                        System.out.println("error reading " + args[i]);
                         break;
                     }
                 }
-                catch (FileNotFoundException e)
-                {  System.out.println("file " + args[i] + " not found");
+                catch (FileNotFoundException e) {
+                    System.out.println("file " + args[i] + " not found");
                     break;
                 }
         }
