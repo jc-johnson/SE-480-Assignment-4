c107ec82506bd4a98b5bf62344163edf364bd435
jc-johnson <jc-johnson@users.noreply.github.com>
2017-11-05 17:52:52 -0800

    added logic to capture run time for filters

--- src/main/java/Application/Application.java
+++ src/main/java/Application/Application.java
@@ -19,23 +19,35 @@ public class Application {
         Buildable buildable = new Reader(new BuildableImpl());
         List<String> stopWords = buildable.buildStopWords(Constants.STOP_WORDS_FILE);
 
-        long startTime = System.currentTimeMillis();
+        long totalStartTime = System.currentTimeMillis();
 
         // Remove stop-words
         WordRemover wordRemover = new WordRemover(stopWords);
+        long wordRemoverStartTime = System.currentTimeMillis();
         wordRemover.filterFile(filePath);
+        long wordRemoverEndTime = System.currentTimeMillis();
+        long totalWordRemoverDuration = wordRemoverEndTime - wordRemoverStartTime;
 
         // Apply stemming algorithm
         Stemmer stemmer = new Stemmer();
+        long stemmerFilterStartTime = System.currentTimeMillis();
         stemmer.filter(Constants.WORD_REMOVAL_OUTPUT_FILE);
+        long stemmerFilterEndTime = System.currentTimeMillis();
+        long totalStemmerFilterDuration = stemmerFilterEndTime - stemmerFilterStartTime;
 
         // Get most commonly used words
         DataSink dataSink = new DataSink();
+        long dataSinkStartTime = System.currentTimeMillis();
         dataSink.run(Constants.STEMMER_OUTPUT_FILE);
+        long dataSinkEndTime = System.currentTimeMillis();
+        long totalDataSinkDuration = dataSinkEndTime - dataSinkStartTime;
         dataSink.printTopWordCount(10);
 
-        long endTime = System.currentTimeMillis();
-        long duration = endTime - startTime;
-        System.out.println("Duration: " + duration);
+        long totalEndTime = System.currentTimeMillis();
+        long totalDuration = totalEndTime - totalStartTime;
+        System.out.println("Total duration: " + totalDuration);
+        System.out.println("Total duration for word remover filter: " + totalWordRemoverDuration);
+        System.out.println("Total duration for stemmer filter: " + totalStemmerFilterDuration);
+        System.out.println("Total duration for data sink filter: " + totalDataSinkDuration );
     }
 }
